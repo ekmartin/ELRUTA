6d47c0ce9b320aa4ec13dcc7fda299cfa73c28e7
Hanse <hanse19@gmail.com>
2014-03-17 17:59:20 +0100

    new dezign

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,27 @@
 require('./graph');
 
-$('.ui.accordion').accordion();
+var app = angular.module('powerhack', []);
 
-var powerhackApp = angular.module('powerhack', []);
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.categories = {
+    water: {
+      name: 'water',
+      info: 'Dusj mindre, eller dusj sammen.',
+      title: 'Vann'
+    },
+    warming: {
+      info: 'Knull mer, bruk ullfrotté aka bukse, under, lang.',
+      title: 'Oppvarming'
+    },
+    electronics: {
+      info: 'Play less, walk more.',
+      title: 'Hjemmeelektronikk'
+    }
+  }
+
+  $scope.currentCategory = null;
+
+  $scope.switchCategory = function(category) {
+    $scope.currentCategory = $scope.categories[category];
+  };
+}]);
