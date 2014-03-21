221a44f711c9ad1374a3d67483196bad7d63d791
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-03-21 16:54:30 +0100

    Add angular rest get

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,18 +7,34 @@ $('.ui.accordion').accordion();
 
 var powerhackApp = angular.module('powerhack', []);
 
+powerhackApp.run(function($rootScope) {
 
-powerhackApp.controller('demo-steinskjer', ['$scope', '$http',
-    function($scope, $http) {
+  $rootScope.meter = '0e6e348bfdb74432b6709526527c3d12';
+  $rootScope.seriesType = 'ActivePlus';
 
-        $http({method: 'GET', url: 'http://google.no'}).
-            success(function(data, status, headers, config) {
 
-                alert('data');
+  // Savings
+  $rootScope.saving = {
+    dateFrom: '2012-01-01',
+    dateTo: '2014-03-20',
+    intervalType: 'Day'
+  };
 
-            }).
-            error(function(data, status, headers, config) {
-            });
+});
+
+
+powerhackApp.controller('demo-steinskjer', ['$scope', '$http', '$rootScope',
+    function($scope, $http, $rootScope) {
+
+        $http({method: 'GET', url: '/api/demo-steinskjer.json?meter=' + $rootScope.meter + '&seriesType=' + $rootScope.seriesType + '&dateFrom=' + $rootScope.saving.dateFrom + '&dateTo=' + $rootScope.saving.dateTo + '&intervalType=' + $rootScope.saving.intervalType})
+          .success(function(data, status, headers, config) {
+
+                alert(data);
+
+          })
+          .error(function(data, status, headers, config) {
+
+          });
 
 
     }]);
