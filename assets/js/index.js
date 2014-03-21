a27abd01db3e7964d4ec9dc7b06387f086e6685d
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-03-21 16:20:59 +0100

    Added demo-steinskjer api

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,23 @@ require('./graph');
 
 $('.ui.accordion').accordion();
 
+
+
+
 var powerhackApp = angular.module('powerhack', []);
+
+
+powerhackApp.controller('demo-steinskjer', ['$scope', '$http',
+    function($scope, $http) {
+
+        $http({method: 'GET', url: 'http://google.no'}).
+            success(function(data, status, headers, config) {
+
+                alert('data');
+
+            }).
+            error(function(data, status, headers, config) {
+            });
+
+
+    }]);
