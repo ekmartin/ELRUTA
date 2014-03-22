c491fda456250cff3cca65dd8a7a3a3f5f1037f5
ekmartin <ekmartinb@gmail.com>
2014-03-22 01:07:44 +0100

    Add realtime graph

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,9 +1,56 @@
 var data;
+var realtimeData;
 var chart;
+var realtimeChart;
 
 var changeGraph = 2;
 
+exports.addRealtimeGraph = function(json) {
+  chart.remove();
+
+  realtimeData = [
+    {
+      key: 'Realtime',
+      color: '#00ff00',
+      values: []
+    }
+  ];
+
+  for (var i = json.length - 20; i < json.length; i++) {
+    var timeStamp = moment(json[i].timeStamp);
+    realtimeData[0].values.push({
+      x: timeStamp.unix(),
+      y: json[i].value
+    });
+  }
+
+  nv.addGraph(function() {
+    realtimeChart = nv.models.lineWithFocusChart()
+      .options({
+        showXAxis: true,
+        showYAxis: true,
+        transitionDuration: 250,
+        margin: {left: 100, bottom: 50}
+      });
+
+    //chart.useInteractiveGuideline(true);
+    chart.xAxis
+      .axisLabel('Klokkeslett')
+      .tickFormat(function(d) {
+          return d3.time.format('%H:%M')(new Date(d*1000));
+      });
+    chart.yAxis
+      .axisLabel('Strømforbruk (kW/h)')
+      .tickFormat(d3.format('d'));
+    });
+
+    d3.select('#graph')
+        .datum(realtimeData)
+        .call(realtimeChart);
+};
+
 exports.addLineGraph = function(json) {
+  //realtimeChart.remove();
   data = [
     {
       key: 'History',
@@ -88,11 +135,13 @@ exports.updateData = function(change) {
   }
 
   for (var j in data[changeGraph].values) {
-    data[changeGraph].values[j].y -= change;
+    data[changeGraph].values[j].y *= change;
   }
 
   console.log(data);
   chart.update();
 };
 
-$('#test').click(function() { console.log("hei");exports.updateData(10);});
+
+
+//$('#test').click(function() { console.log("hei");exports.updateData(10);});
